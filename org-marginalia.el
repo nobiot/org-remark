f9c313342ca9ba34e0e2962f10e82f94777859a4
Noboru Ota <me@nobiot.com>
2021-08-15 15:11:17 +0200

    chg: separate global-tracking.el
    This way, you don't need to load the whole Org machinary to turn on global
    tracking in init.

--- org-marginalia.el
+++ org-marginalia.el
@@ -169,6 +169,7 @@
 
 (require 'org)
 (require 'org-id)
+(require 'org-marginalia-global-tracking)
 (declare-function org-collect-keywords 'org)
 
 ;;;; Customization
@@ -197,13 +198,6 @@ Ensure that it is an Org file."
   :type 'string
   :group 'org-marginalia)
 
-(defcustom org-marginalia-tracking-file
-  (locate-user-emacs-file ".org-marginalia-tracking" nil)
-  "File name where the files `org-marginalia' tracks is saved.
-When `org-marginalia-global-tracking-mode' is active, opening a file
-saved in `org-marginalia-tracking-file' automatically loads highlights."
-  :type 'file)
-
 ;;;; Variables
 
 (defvar-local org-marginalia-loaded nil)
@@ -220,10 +214,6 @@ On save-buffer each highlight will be persisted in the marginalia file
   "Stores the cloned indirect buffer for the margin notes.
 It is meant to exist only one of these in each Emacs session.")
 
-(defvar org-marginalia-tracking-file-loaded nil)
-
-(defvar org-marginalia-files-tracked nil)
-
 ;; Const for the names of properties in Org Mode
 (defconst org-marginalia-prop-id "marginalia-id")
 (defconst org-marginalia-prop-source-file "marginalia-source-file")
@@ -231,54 +221,6 @@ It is meant to exist only one of these in each Emacs session.")
 (defconst org-marginalia-prop-source-end "marginalia-source-end")
 
 ;;;; Commands
-
-;;;###autoload
-(define-minor-mode org-marginalia-global-tracking-mode
-  "Global mode. When enabled, check files saved in
-`org-marginalia-tracking-file' and opening them automatically
-activates `org-marginalia-mode' locally for the file opened."
-  :init-value nil
-  :lighter " marginalia-tracking"
-  :global t
-  (cond
-   (org-marginalia-global-tracking-mode
-    ;; Activate
-    (when (and (not org-marginalia-tracking-file-loaded)
-	       (file-exists-p org-marginalia-tracking-file))
-      (org-marginalia-tracking-load))
-    (add-hook 'find-file-hook #'org-marginalia-tracking-auto-on)
-    (add-hook 'kill-emacs-hook #'org-marginalia-tracking-save))
-   (t
-    ;; dactivate
-    (setq org-marginalia-files-tracked nil)
-    (setq org-marginalia-tracking-file-loaded nil)
-    (remove-hook 'find-file-hook #'org-marginalia-tracking-auto-on)
-    (remove-hook 'kill-emacs-hook #'org-marginalia-tracking-save))))
-
-(defun org-marginalia-tracking-auto-on ()
-  (when (and org-marginalia-files-tracked
-	     (member (abbreviate-file-name (buffer-file-name))
-		     org-marginalia-files-tracked))
-    (org-marginalia-mode +1)))
-
-(defun org-marginalia-tracking-load ()
-  ".
-Each line. It loads regardless of `org-marginalia-tracking-file';
-if already loaded this function reloads."
-  (with-temp-buffer
-    (condition-case nil
-	(progn
-	  (insert-file-contents org-marginalia-tracking-file)
-	  (setq org-marginalia-files-tracked
-		(split-string (buffer-string) "\n"))
-          (setq org-marginalia-tracking-file-loaded t)))))
-
-(defun org-marginalia-tracking-save ()
-  "."
-  (interactive)
-  (when org-marginalia-files-tracked
-    (with-temp-file org-marginalia-tracking-file
-      (insert (mapconcat 'identity org-marginalia-files-tracked "\n")))))
 
 ;;;###autoload
 (define-minor-mode org-marginalia-mode
