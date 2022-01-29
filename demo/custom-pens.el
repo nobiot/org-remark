32de4dceb6852ba4d863e181e0c456a3b68e3b37
Noboru Ota <me@nobiot.com>
2022-01-29 17:33:32 +0100

    fix: typo in the title screen shot #11
    https://github.com/nobiot/org-remark/issues/11#issuecomment-1024007854

--- demo/custom-pens.el
+++ demo/custom-pens.el
@@ -2,5 +2,9 @@
                    '(:foreground "white" :underline "black")
                    '(CATEGORY "exam"))
 
-(org-remark-create "magenda"
+(org-remark-create "magenta"
                    '(modus-themes-nuanced-magenta))
+
+(org-remark-create "typo"
+                   '(:underline (:color "#8f0075" :style wave))
+                   '(help-echo "Fix the typo"))
