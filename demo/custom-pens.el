40d0e5fe393c22232a7a18ec722ba2c86bc2d40a
Noboru Ota <me@nobiot.com>
2022-01-30 09:46:36 +0100

    ELPA - for now add README-elpa and full copyright/license notice

--- demo/custom-pens.el
+++ demo/custom-pens.el
@@ -1,5 +1,23 @@
 ;; Copyright (C) 2021-2022  Free Software Foundation, Inc.
 
+;; Author: Noboru Ota <me@nobiot.com>
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 (org-remark-create "memorize"
                    '(:foreground "white" :underline "black")
                    '(CATEGORY "exam"))
