eb793504d50b8bcaa9ea8c5c308962a624677821
Noboru Ota <me@nobiot.com>
2024-09-29 11:32:24 +0200

    style: Moving source header to let time-stamp to work.
    No functional change

--- org-remark.el
+++ org-remark.el
@@ -2,28 +2,30 @@
 
 ;; Copyright (C) 2020-2024 Free Software Foundation, Inc.
 
+;; This program is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation, either version 3 of the License, or (at your
+;; option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License along
+;; with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;; Author: Noboru Ota <me@nobiot.com>
-;; URL: https://github.com/nobiot/org-remark
-;; Version: 1.2.2
 ;; Created: 22 December 2020
-;; Last modified: 29 June 2024
-;; Package-Requires: ((emacs "27.1") (org "9.4"))
+;; Last modified: 29 September 2024
+
+;; URL: https://github.com/nobiot/org-remark
 ;; Keywords: org-mode, annotation, note-taking, marginal-notes, wp,
 
+;; Version: 1.2.2
+;; Package-Requires: ((emacs "27.1") (org "9.4"))
+
 ;; This file is not part of GNU Emacs.
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
